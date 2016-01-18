711a00bc6f2f16d628dcd2781607a90bd889d3cc
Lance Boettcher <lboettch@csclnx11.ad.calpoly.edu>
2016-01-17 16:49:46 -0800

    all structures written. ip done

--- Program1/trace.c
+++ Program1/trace.c
@@ -12,14 +12,25 @@
 #include "trace.h" 
 #include "checksum.h"
 
+#define ETHERNET_LENGTH 14
+#define IP_LENGTH 20
+#define IP_VERSION_LOC 4
+#define DSCP_LOC 5
+#define ICMP_CODE 1
+#define TCP_CODE 6
+#define UDP_CODE 17
+
 int main(int argc, char *argv[]) {
 
    if (argc != 2) {
       fprintf(stderr, "You must provide a trace file");
       exit(-1);
+   }  
+   else {
+      sniffTraceFile(argv[1]);
    }
 
-   sniffTraceFile(argv[1]);
+   return 0;
 }
 
 void sniffTraceFile(char *filename) {
@@ -27,7 +38,8 @@ void sniffTraceFile(char *filename) {
    char errorString[PCAP_ERRBUF_SIZE];
    struct pcap_pkthdr *header;
    const u_char *packet;
-   struct ethernet *ethernet;
+   struct ethernet *ethernetHeader;
+   int packetNumber = 1;
 
    handle = pcap_open_offline(filename, errorString);
 
@@ -37,34 +49,109 @@ void sniffTraceFile(char *filename) {
    }
 
    while (pcap_next_ex(handle, &header, &packet)) {
-      ethernet = (struct ethernet*) packet;
+      printf("Packet number: %d Packet Len: %d\n\n", 
+            packetNumber++, header->len);
 
-      if (ethernet->type == TYPE_IP) {
-         sniffIP(header, packet);
+      /* Ethernet Packet is always first */ 
+      ethernetHeader = (struct ethernet *) packet;
+
+      printf("\tEthernet Header\n\t\tDest MAC: %s\n\t\tSource MAC: %s\n",
+            ether_ntoa(&(ethernetHeader->dest)), 
+            ether_ntoa(&(ethernetHeader->src)));
+
+      if (ethernetHeader->type == TYPE_IP) {
+         printf("\t\tType: IP\n\n");
+         sniffIP(packet + ETHERNET_LENGTH);
       }
-      else if (ethernet->type == TYPE_ARP) {
-         sniffARP(header, packet);
+      else if (ethernetHeader->type == TYPE_ARP) {
+         printf("\t\tType: ARP\n\n");
+         sniffARP(packet + ETHERNET_LENGTH);
       }
       else {
-         sniffUnknown(header, packet);
+         printf("\t\tType: Unknown\n\n");
+         sniffUnknown(packet + ETHERNET_LENGTH);
       }
    }   
 
    pcap_close(handle);
 }  
 
-void sniffIP(u_char *header, u_char *packet) {
+void sniffIP(u_char *loc) {
 
+   printf("\tIP Header\n");
+
+   ip ipHeader = (struct ip *) loc;
+
+   /* 
+    * Version and Length
+    * 0 | IP Version | 4 | IP Header Length | 7 
+    */ 
+   printf("\t\tIP Version: %u\n", 
+         ipHeader->versionAndLength >> IP_VERSION_LOC);
+   printf("\t\tHeader Len (bytes): %u\n", 
+         ipHeader->versionAndLength & 0x0F);
+
+   /* 
+    * TOS
+    * 0 | DSCP | 5 | ECN | 7 
+    */ 
+   printf("\tTOS subfields:\n");
+   printf("\t\tDiffserv bits: %u\n", ipHeader->tos >> DSCP_LOC);
+   printf("\t\tECN bits: %u\n", ipHeader->tos & 0x03);
+   
+   /* TTL */ 
+   printf("\t\tTTL: %u\n", ipHeader->ttl);
+
+   /* Protocol */ 
+   if (protocol == ICMP_CODE) 
+      printf("\t\tProtocol: ICMP\n");
+   else if (protocol == TCP_CODE) 
+      printf("\t\tProtocol: TCP\n");
+   else if (protocol == UDP_CODE)
+      printf("\t\tProtocol: UDP\n");
+   else 
+      printf("\t\tProtocol: Unknown\n");
+
+   /* Checksum */ 
+
+
+   /* Sender IP */ 
+   printf("\t\tSender IP: %s\n", inet_ntoa(ipHeader->src));
+
+   /* Dest IP */ 
+   printf("\t\tDest IP: %s\n", inet_ntoa(ipHeader->dest));
+
+   sniffProtocol(protocol, loc + IP_LENGTH);
+}
+
+void sniffARP(u_char *loc) {
+
+   ip ipHeader = (struct ip *) 
+
+}
+
+void sniffUnknown(u_char *loc) {
 
 
 }
 
-void sniffARP(u_char *header, u_char *packet) {
+void sniffProtocol(u_char type, u_char *loc) {
+
+   switch (type) {
+      case ICMP_CODE: 
 
 
-}
 
-void sniffUnknown(u_char *header, u_char *packet) {
+         break;
+      case TCP_CODE: 
 
+
+         break;
+      case UDP_CODE: 
+
+
+         break;
+      default: 
+   }
 
 }
--- Program1/trace.h
+++ Program1/trace.h
@@ -6,32 +6,97 @@
 
 #define TYPE_IP 0x0800
 #define TYPE_ARP 0x0806
-#define 
 
 struct ethernet {
 
-   u_char dest[ETHERNET_LENGTH];
-   u_char src[ETHERNET_LENGTH];
+   struct ether_addr dest;
+   struct ether_addr src;
    u_short type;
 
 }__attribute__((packed));
 
 struct ip {
 
+   u_char versionAndLength;
+   u_char tos;
+   u_short size;
+   u_short id;
+   u_short fragOffset;
+   u_char ttl;
+   u_char protocol;
+   u_short checksum;
+   struct in_addr src;
+   struct in_addr dest;
 
 }__attribute__((packed));
 
 struct tcp {
 
+   u_short sourcePort;
+   u_short destPort;
+   uint32_t sequenceNum;
+   uint32_t ackNum;
+   u_char dataOffset;
+   u_char flags;
+   u_short window;
+   u_short checksum;
+   u_short urgent;
 
 }__attribute__((packed));
 
 struct udp {
 
+   uint16_t src;
+   uint16_t dest;
+   uint16_t length;
+   uint16_t checksum;
 
 }__attribute__((packed));
 
 struct arp {
 
+   uint16_t hardwareType;
+   uint16_t protocolType;
+   u_char hardwareAddressLength;
+   u_char protocolAddressLength;
+   uint16_t opcode;
+   struct ether_addr senderHardwareAddress;
+   struct in_addr senderProtocolAddress;
+   struct ether_addr targetHardwareAddress;
+   struct in_addr targetProtocolAddress;
 
 }__attribute__((packed));
+
+struct icmp {
+
+   u_char type;
+   u_char code;
+   uint16_t checksum;
+   uint32_t restOfHeader;
+
+}__attribute__((packed));
+
+struct tcp {
+
+   uint16_t src;
+   uint16_t dest;
+   uint32_t sequenceNumber;
+   uint32_t ackNumber;
+   u_char offsetAndReserved;
+   u_char flags;
+   uint16_t windowSize;
+   uint16_t checksum; 
+   uint16_t urgent; //Maybe not?
+   uint32_t padding;
+
+}__attribute__((packed));
+
+struct udp {
+
+   uint16_t src;
+   uint16_t dest;
+   uint16_t length;
+   uint16_t checksum;
+
+}__attribute__((packed));
+ 
